2bc537add7d7db748b3f02894e19a3c4865e02f0
임준서 <limjunseo123123@gmail.com>
2025-07-25 20:46:27 +0900

    [feat] : cicd 테스트-v2

--- src/main/java/com/example/team4/test/TestController.java
+++ src/main/java/com/example/team4/test/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "Test successful!";
+        return "Test successful!!v2";
     }
 }
